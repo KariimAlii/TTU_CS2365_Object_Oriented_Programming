0113f352d54d09da5590c5a4eba966c10d93591c
sdevaneyTTU <57376844+sdevaneyTTU@users.noreply.github.com>
2020-02-25 21:33:19 -0600

    Add files via upload

---
+++ UNOCard.java
@@ -0,0 +1,116 @@
+/*
+ * TITLE: Card Class for Project 2 UNO Workout
+ * AUTHOR: Stephen C. Devaney
+ * FOR: CS 2365 Object Oriented Programming Section 001 Spring 2020
+ */
+
+
+package unogame;
+/**
+ *
+ * @author Stephen C. Devaney
+ */
+public class UNOCard {
+    public final static int BLUECARD = 0, GREENCARD = 1, REDCARD = 2, YELLOWCARD = 3, BLACKCARD = 4; // constants for the card colors
+    public final static int SKIP = 10, DRAW2 = 11, REVERSE = 12, WILD = 13, WILDDRAW4 = 14; // constants for action cards
+    private int color; // color of the card
+    private int number; // number of the card
+    
+    
+    /** UNOCard Base Constructor
+     * @param color
+     * @param number
+     */
+    public UNOCard(int color, int number){
+        if ((0 <= number && number <= REVERSE) && (BLUECARD <= color && color <= YELLOWCARD)) // color cards
+        {
+            this.color = color;
+            this.number = number;
+        }
+        else if ((WILD <= number && number <= WILDDRAW4) && color == BLACKCARD) // wild cards
+        {
+            this.color = color;
+            this.number = number;
+        }
+    }
+    
+    
+    /** cardToString method
+     * ADDITIONAL PARAMETER: the card needs to be initialize with proper values for UNOCards based upon the constructor
+     * @return String
+     * DESCRIPTION: the method takes a card and outputs the proper wording for the card in a string
+     */
+    public String cardToString(){
+        String returnString = "";
+        switch (this.color) {
+            case BLUECARD:
+                returnString += "Blue ";
+                break;
+            case GREENCARD:
+                returnString += "Green ";
+                break;
+            case REDCARD:
+                returnString += "Red ";
+                break;
+            case YELLOWCARD:
+                returnString += "Yellow ";
+                break;
+            default:
+                break;
+        }
+        if (this.color != BLACKCARD){
+            if (0 <= this.number && this.number <= 9) {returnString += this.number;}
+            else if (this.number == SKIP) {returnString += "Skip";}
+            else if (this.number == DRAW2) {returnString += "Draw Two";}
+            else if (this.number == REVERSE) {returnString += "Reverse";}
+        }
+        else
+        {
+            returnString += "Wild";
+            if (this.number == 14) {returnString += " Draw 4";}
+        }
+        return returnString;
+    }
+    
+    
+    /** cardToString method
+     * @param secondcard: UNOCard
+     * ADDITIONAL PARAMETER: the card needs to be initialize with proper values for UNOCards based upon the constructor
+     * @return int
+     * DESCRIPTION: compares this card with the second card 
+     *                  if this card goes before the second card -1 is returned
+     *                  if this card goes after the second card 1 is returned
+     *                  if this card is equal to the second card 0 is returned
+     */
+    public int compareCard(UNOCard secondcard){
+        int result;
+        if (this.color < secondcard.color) {result = -1;}
+        else if (this.color > secondcard.color) {result = 1;}
+        else{
+            if (this.number < secondcard.number) {result = -1;}
+            else if (this.number > secondcard.number) {result = 1;}
+            else {result = 0;}
+        }
+        return result;
+    }
+    
+    
+    /** getCardColor method
+     * ADDITIONAL PARAMETER: the card needs to be initialize with proper values for UNOCards based upon the constructor
+     * @return int
+     * DESCRIPTION: returns the color value of the card
+     */
+    public int getCardColor(){
+        return this.color;
+    }
+    
+    
+    /** getCardColor method
+     * ADDITIONAL PARAMETER: the card needs to be initialize with proper values for UNOCards based upon the constructor
+     * @return int
+     * DESCRIPTION: returns the number value of the card
+     */
+    public int getCardNumber(){
+        return this.number;
+    }
+}
