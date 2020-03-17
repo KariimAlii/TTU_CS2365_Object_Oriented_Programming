3cc092a6fc5d78a526055d261fc9061519a8fd2d
sdevaneyTTU <57376844+sdevaneyTTU@users.noreply.github.com>
2020-03-17 10:07:00 -0500

    Update BangDie.java

--- BangTheDiceGame/BangDie.java
+++ BangTheDiceGame/BangDie.java
@@ -4,7 +4,7 @@
  * FOR: CS 2365 Object Oriented Programming Section 001 Spring 2020
  */
 package bangthedicegame;
-
+import java.util.Random;
 /**
  *
  * @author Stephen C. Devaney
@@ -16,30 +16,17 @@ public class BangDie {
     
     
     /** BangDie Base Constructor
-     * test case 1: always sets symbol to -1 and processedState to true
+     * test case 1: always sets symbol to 1 and processedState to false
      */
     public BangDie(){
-        this.symbol = -1;
-        this.processedState = true;
-    }
-    
-    
-    
-    /** isNullDie method
-     * DESCRIPTION: checks to see if the die is NULL
-     * test case 1: die is group designated null returns true
-     * test case 2: die is not group designated null returns false
-     * @return boolean
-     */
-    public boolean isNullDie(){
-        return(this.symbol == -1);
+        this.symbol = ARROW;
+        this.processedState = false;
     }
     
     
     /** dieToString method
      * DESCRIPTION: the method takes a die and outputs the proper wording for the die in a string
-     * test case 1: non null die returns the proper string
-     * test case 2: null die returns a null string
+     * test case 1: returns the proper string for the die
      * @return String
      */
     public String dieToString(){
@@ -83,20 +70,17 @@ public class BangDie {
      */
     public int compareDie(BangDie die){
         int result;
-        if(!this.isNullDie()){
-            if (this.isEqual(die)) {result = 0;}
-            else if (this.symbol < die.symbol) {result = -1;}
-            else{result = 1;}
-        }
-        else return 1;
+        if (this.isEqual(die)) {result = 0;}
+        else if (this.symbol < die.symbol) {result = -1;}
+        else{result = 1;}
         return result;
     }
     
     
     /** isEqual method
      * DESCRIPTION: compares this die with the second die if this die is equal to the second die true is returned otherwise false is returned
-     * test case 2: first die equals second die returns true
-     * test case 3: first die does not equal the second die returns false
+     * test case 1: first die equals second die returns true
+     * test case 2: first die does not equal the second die returns false
      * @param die: BangDie
      * @return boolean
      */
@@ -106,9 +90,8 @@ public class BangDie {
     
     
     /** getDieSymbol method
-     * DESCRIPTION: returns the color value of the die
-     * test case 1: non null die returns the value of the color
-     * test case 2: null die returns -1
+     * DESCRIPTION: get the symbol value of the die
+     * test case 1: returns the value of the symbol of the die
      * @return int
      */
     public int getDieSymbol(){
@@ -117,9 +100,8 @@ public class BangDie {
     
     
     /** getDieState method
-     * DESCRIPTION: returns the state of the die
-     * test case 1: non null die returns the value of the state
-     * test case 2: null die returns true
+     * DESCRIPTION: get the state of the die
+     * test case 1: returns the value of the state of the die
      * @return boolean
      */
     public boolean getDieState(){
@@ -128,37 +110,30 @@ public class BangDie {
     
     
     /** setDieState method
-     * DESCRIPTION: returns the color state of the die
-     * test case 1: sets the value of the state
-     * @param state state to be changed to
+     * DESCRIPTION: process the die
+     * test case 1: sets the value of the state to true
      */
-    public void setDieState(boolean state){
-        this.processedState = state;
+    public void processDie(){
+        this.processedState = true;
     }
     
 
     /** setDieSymbol method
-     * DESCRIPTION: returns the color state of the die
-     * test case 1: sets the value of the state if have a proper value
-     * test case 2: other wise set to a null dice
-     * @param value value to be changed to
+     * DESCRIPTION: rolls the die to a random symbol
+     * test case 1: sets the value of the state to false and the symbol to a proper symbol
      */
-    public void setDieSymbol(int value){
-        if ((ARROW <= value && value <= GATLING)){
-            this.symbol = value;
-            this.processedState = false;
-        }
-        else{
-            this.symbol = -1;
-            this.processedState = true;
-        }
+    public void rollDie(){
+        Random rand = new Random(System.currentTimeMillis());
+        BangDie newdie = new BangDie();
+        int random = rand.nextInt(GATLING - ARROW + 1) + ARROW;
+        this.symbol = random;
+        this.processedState = false;
     }
     
     
     /** copyDie method
      * DESCRIPTION: copies a dice value onto another die best use in the a dice class.
-     * test case 1: non null die is copied exactly
-     * test case 2: null die is copied exactly
+     * test case 1: die is copied exactly
      * @param die is the die to be copied from
      */
     public void copyDie(BangDie die){
