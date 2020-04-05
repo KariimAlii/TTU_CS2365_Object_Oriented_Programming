48aa8c401e029ec15dae46c831b77db32768e905
Steven-Lowry <61514859+Steven-Lowry@users.noreply.github.com>
2020-04-04 20:33:04 -0500

    Add files via upload

---
+++ BangTheDiceGame/GUI/Bang_GUI.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bang_gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Steven
+ */
+public class Bang_GUI extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Game Board.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("BANG - The Dice Game");
+        stage.getIcons().add(new Image("/Images/bang .jpg"));
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
