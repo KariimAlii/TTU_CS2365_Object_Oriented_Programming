aef665a720492a4245eff6a50438ecb3158bdb5c
Anamol Acharya <anamol222@gmail.com>
2020-05-02 18:57:27 -0500

    New character for expansion two.

---
+++ src/character/GregDigger.java
@@ -0,0 +1,13 @@
+/*
+ * TITLE: Project 3 Bang The Dice Game
+ * AUTHOR: Anamol Acharya
+ * FOR: CS 2365 Object Oriented Programming Section 001 Spring 2020
+ */
+package character;
+
+
+public class GregDigger {
+    
+    //You may use each  whisket bottle to roll twice.
+    
+}
