0d5668457890a5ef0e7b8c888696d00fcfd26a6d
Anamol Acharya <anamol222@gmail.com>
2020-05-02 18:55:59 -0500

    Created separate method for coward and loud method and New characted for expansion two.

---
+++ src/character/BelleStar.java
@@ -0,0 +1,14 @@
+/*
+ * TITLE: Project 3 Bang The Dice Game
+ * AUTHOR: Anamol Acharya
+ * FOR: CS 2365 Object Oriented Programming Section 001 Spring 2020
+ */
+package character;
+
+
+public class BelleStar {
+    
+    //After each of your dice rolls, you can change
+    //one Dynamite to Gatlings (Not if you roll three or more Gatling Guns !).
+    
+}
