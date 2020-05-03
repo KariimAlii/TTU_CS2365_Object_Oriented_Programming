dabebd688274cfcf2b569712f51ba4518de725e6
Anamol Acharya <anamol222@gmail.com>
2020-05-02 19:18:55 -0500

    New character for expansion two.

--- src/character/BelleStar.java
+++ src/character/BelleStar.java
@@ -5,10 +5,48 @@
  */
 package character;
 
+import javafx.scene.image.Image;
 
-public class BelleStar {
+
+public class BelleStar extends BangCharacter {
     
-    //After each of your dice rolls, you can change
-    //one Dynamite to Gatlings (Not if you roll three or more Gatling Guns !).
+     private static final String NAME = "BelleStar";
+    private static final String SPECIAL = "After each of your dice rolls, you can change one Dynamite to Gatlings";
+    // Todo
+    private static final String IMAGEFILENAME = "   ";
+
+    
+    /**
+     * Constructor for BelleStar calls the super constructor from BangCharacter Class
+     */
+    public BelleStar(){
+        super(8);
+    }
+    
+    /**
+     * Method to get the name of the character.
+     * @return
+     */
+    @Override
+    public String getName(){
+        return NAME;
+    }
+    
+    /**
+     * Method to get the character's special ability in the form of a string so that
+     * it may be displayed
+     * @return
+     */
+    @Override
+    public String getSpecial(){
+        return SPECIAL;
+    }
+    
+    @Override
+    public String getImageFileName(){
+        return IMAGEFILENAME;
+    }
+    
+  
     
 }
