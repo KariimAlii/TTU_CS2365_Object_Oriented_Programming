65eba1b3c93711f876a901d1b52cabd064298692
Anamol Acharya <anamol222@gmail.com>
2020-05-02 19:18:30 -0500

    New character for expansion two.

--- src/character/GregDigger.java
+++ src/character/GregDigger.java
@@ -6,8 +6,44 @@
 package character;
 
 
-public class GregDigger {
+public class GregDigger extends BangCharacter{
     
-    //You may use each  whisket bottle to roll twice.
+    private static final String NAME = "GregDigger";
+    private static final String SPECIAL = "You may use each  whisket bottle to roll twice";
+    // Todo
+    private static final String IMAGEFILENAME = "   ";
+
+    
+    /**
+     * Constructor for GregDigger calls the super constructor from BangCharacter Class
+     */
+    public GregDigger(){
+        super(7);
+    }
+    
+    /**
+     * Method to get the name of the character.
+     * @return
+     */
+    @Override
+    public String getName(){
+        return NAME;
+    }
+    
+    /**
+     * Method to get the character's special ability in the form of a string so that
+     * it may be displayed
+     * @return
+     */
+    @Override
+    public String getSpecial(){
+        return SPECIAL;
+    }
+    
+    @Override
+    public String getImageFileName(){
+        return IMAGEFILENAME;
+    }
+    
     
 }
