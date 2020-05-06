cdd466e76f0de7dcfc90910ae0ff0aed77de6f86
Stephen Devaney <stephen.c.devaney@ttu.edu>
2020-05-06 12:01:01 -0500

    Fixed bugs with roll dice simulation

--- src/player/Player.java
+++ src/player/Player.java
@@ -909,4 +909,8 @@ public abstract class Player {
         this.turnoutput += "\nAfter reading this player's turn output please click End Turn!\n";
         return this.turnoutput;
     }
+    
+    public boolean hasIndianChiefArrow(){
+        return this.hasIndianChiefArrow;
+    }
 }
