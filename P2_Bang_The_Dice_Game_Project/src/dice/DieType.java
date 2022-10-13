3739e975b92d855d9ca838d42bcabc7b445ee63a
stephencdevaney <stephen.c.devaney@ttu.edu>
2022-10-13 15:01:35 -0500

    Github file reorganization

---
+++ P2_Bang_The_Dice_Game_Project/src/dice/DieType.java
@@ -0,0 +1,17 @@
+/*
+ * TITLE: Project 3 Bang The Dice Game
+ * AUTHOR: Stephen C. Devaney
+ * FOR: CS 2365 Object Oriented Programming Section 001 Spring 2020
+ */
+package dice;
+
+/**
+ *  DESCRIPTION: Die enum type
+ * @author Stephen C. Devaney
+ */
+public enum DieType {
+    BASIC,
+    LOUDMOUTH,
+    COWARD,
+    DUEL
+}
